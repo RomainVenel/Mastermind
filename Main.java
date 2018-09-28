1071d1bd8fc7dbab958536f36d629aaf2524fd1e
Romain Venel <romain.venel.sio@gmail.com>
2018-09-28 20:48:14 +0200

    [Refactor] Refactor du code en fonctions
    -Generate javadoc

---
+++ Main.java
@@ -0,0 +1,22 @@
+/**
+ * The Class Main.
+ */
+public class Main {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Bienvenue dans le jeu du Mastermind !"
+				+ "\nUn nombre aléatoire de 4 chiffres a été généré, vous avez 8 essais pour le trouver !"
+				+ "\nBonne chance !");
+		
+		Mastermind game = new Mastermind();
+
+	}
+
+}
--- Mastermind.java
+++ Mastermind.java
@@ -6,160 +6,256 @@ import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * The Class Mastermind.
+ */
 public class Mastermind {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
+	/** Temps au lancement du jeu. */
+	private long secsEarly = 0;
+	
+	/** Temps à la fin du jeu. */
+	private long secsEnd = 0;
+	
+	/** Nom d'emplacement des fichiers. */
+	private String savestr = "history.txt"; 
+	
+	/** Fichier d'historique des parties. */
+	private File f = new File(savestr);
+	
+	/** Ecriture dans le fichier. */
+	private PrintWriter out = null;
+	
+	/** Initialisation du scanner. */
+	Scanner sc = new Scanner(System.in);
+	
+	/** The array user. */
+	int[] arrayUser = new int[4];
+	
+	/** The array random. */
+	int[] arrayRandom = new int[4];
+	
+	/** The array final. */
+	String[] arrayFinal = new String[4];
+	
+	/** The exact digit. 
+	 * Le jeu s'arrête si la variable vaut 4.*/
+	int exactDigit = 0;
+	
+	/** Compteur d'essais. */
+	int compt = 1;
+	
+	/**
+	 * Instantiates a new mastermind.
+	 */
+	public Mastermind() {
 		
-		boolean replay = true;
-		long secsEarly = 0;
-		long secsEnd = 0;
-		String savestr = "filename.txt"; 
-		File f = new File(savestr);
-
-		PrintWriter out = null;
-		if ( f.exists() && !f.isDirectory() ) {
-		    out = new PrintWriter(new FileOutputStream(new File(savestr), true));
-		}
-		else {
-		    out = new PrintWriter(savestr);
-		}
-			
-		do {
-			
-			// Timer au début du jeu
-			secsEarly = (new Date().getTime())/1000;
-			
-			// Compteur de coups
-			int compt = 1;
-		
-			// Le jeu s'arrête si la variable vaut 4
-			int exactDigit = 0;
-			
-			// Génère un nombre aléatoire entre 0000 et 9999
-			Random r = new Random();
-			int randomNumber = r.nextInt(10000);
-			String realNumber = String.format("%04d", randomNumber);
-			
-			// append the sequence
-			out.println("----------------------------------------------------");
-		    out.println("Le nombre à trouver est: " + realNumber);
-			
-			// Récupère la saisie de l'utilisateur
-			Scanner sc = new Scanner(System.in);
-			
-			// Tableau où est stocké le nombre aléatoire
-			int[] arrayRandom = new int[4];
-		
-			// Tableau où est stocké le nombre saisi par l'utilisateur
-			int[] arrayUser = new int[4];
-			
-			// Tableau où est stocké le résultat à la fin de chaque essai
-			String[] arrayFinal = new String[4];
-			
-			// Remplissage du tableauRandom avec le nombre random
-			for (int i=0; i < arrayRandom.length; i++)
-			{
-			  arrayRandom[i] = Character.getNumericValue(realNumber.charAt(i));
-			}
-			
-			System.out.println("Bienvenue dans le jeu du Mastermind !\nUn nombre aléatoire de 4 chiffres a été généré, vous avez 8 essais pour le trouver !\nBonne chance !");
-			System.out.println("\nVoici le nombre aléatoire : " + realNumber);
-			
-			do {
-			
-				exactDigit = 0;
-				
-				System.out.println("\nEssai " + compt + ": ");
-				String str = sc.nextLine();
-				System.out.println("Vous avez saisi : " + str);
-				
-				// append the sequence
-			     out.println("Essai " + compt + ": " + str);
-
-				// Vérification si la saisie est un nombre de 4 caractères
-				if (str.matches("[a-zA-Z]+") || str.length() != 4) {
-					
-					System.out.println("Nombre incorrect !");
-					continue;
-					
-				}else {
-					
-					// Remplissage du tableauUser avec la saisie de l'utilisateur
-					for(int i = 0; i < 4; i++) {
-						arrayUser[i] = Character.getNumericValue(str.charAt(i));
-					}
-					
-					// On parcourt les 2 tableaux et on vérifie chaque entrée
-					for(int i = 0; i < 4; i++) {
-						for(int j = 0; j < 4; j++) {
-							
-							if (arrayUser[i] == arrayRandom[j]) {
-								if (i == j) {
-									exactDigit++;
-									arrayFinal[i] = " + ";
-									
-									break;
-								}else {
-									if (arrayFinal[i] != " + ") {
-										arrayFinal[i] = " 1 ";
-									}
-								}
-							}else {
-								if (arrayFinal[i] != " + " && arrayFinal[i] != " 1 ") {
-									arrayFinal[i] = " - ";
-								}
-							}
-							
-						}
-					}
-					
-					// Affichage du résultat final après chaque essai
-					for(int k = 0; k < 4; k++) {
-						System.out.print(arrayFinal[k]);
-					}
-					
-					if (exactDigit == 4) {
-						System.out.println("\nBravo ! Vous avez gagné en " + compt + " coups !");
-						// Timer à la fin du jeu
-						secsEnd = (new Date().getTime())/1000;
-						System.out.println("Vous avez mis " + (secsEnd - secsEarly) + " secondes !");
-						out.println("Gagné en " + compt + " coups et " + (secsEnd - secsEarly) + " secondes");
-						out.close();
-						break;
-					}else {
-						// Réinitialisation du tableau final
-						arrayFinal = new String[4];
-					}
-					
-				}
-				compt++;
-				
-				
-				
-			} while (compt < 9);
-		
-			if (compt >= 9 ) {
-				System.out.println("\nVous avez perdu !");
-				// Timer à la fin du jeu
-				secsEnd = (new Date().getTime())/1000;
-				System.out.println("\nVous avez mis " + (secsEnd - secsEarly) + " secondes !");
-				out.println("Perdu !");
-				out.close();
-			}
-			
-			System.out.println("Voulez-vous rejouer ? 1 pour oui, 2 pour non");
-			int rep = sc.nextInt();
-			
-			if (rep == 1) {
-				replay = true;
-			}else {
-				replay = false;
-				System.out.println("Fin du jeu !");
-			}
-			
-		}while(replay == true);
+		play();
 		
 	}
+	
+	/**
+	 * Play the game.
+	 * Check if file exist,
+	 * Generate random number,
+	 * Get user number
+	 */
+	private void play() {
+		
+		try {
+			checkIfFileExist();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		generateRandom();
+		getNumberUser();
+		
+	}
+	
+	/**
+	 * Generate random number between 0000 et 9999.
+	 */
+	private void generateRandom() {
+		
+		// Timer au début du jeu
+		this.secsEarly = (new Date().getTime())/1000;
+		
+		// Initialisation du compteur à 1
+		this.compt = 1;
+		
+		// Initialisation du tableau de random numbers
+		this.arrayRandom = new int[4];
+		
+		Random r = new Random();
+		int randomNumber = r.nextInt(10000);
+		String realNumber = String.format("%04d", randomNumber);
+		
+		System.out.println("Le nombre à trouver est " + realNumber);
+		
+		//
+		this.out.println("----------------------------------------------------");
+	    this.out.println("Le nombre à trouver est: " + realNumber);
+		
+		// Remplissage du tableauRandom avec le nombre random
+		for (int i=0; i < arrayRandom.length; i++)
+		{
+			arrayRandom[i] = Character.getNumericValue(realNumber.charAt(i));
+		}
+		
+	}
+	
+	/**
+	 * Gets the number user.
+	 *
+	 * @return the number user
+	 */
+	private String getNumberUser() {
 
+		// Tableau où est stocké le nombre saisi par l'utilisateur
+		this.arrayUser = new int[4];
+		
+		System.out.println("\nEssai " + this.compt + ": ");
+		String str = sc.nextLine();
+		System.out.println("Vous avez saisi : " + str);
+		
+		this.out.println("Essai " + compt + ": " + str);
+		
+		checkNumberUser(str);
+		
+		return str;
+		
+	}
+	
+	/**
+	 * Check number user.
+	 *
+	 * @param userNumber the user number
+	 */
+	private void checkNumberUser(String userNumber) {
+		
+		// Vérification si la saisie est un nombre de 4 caractères
+		if (userNumber.matches("[a-zA-Z]+") || userNumber.length() != 4) {
+			
+			System.out.println("Nombre incorrect !");
+			getNumberUser();
+			
+		}else {
+			
+			// Remplissage du tableauUser avec la saisie de l'utilisateur
+			for(int i = 0; i < 4; i++) {
+				this.arrayUser[i] = Character.getNumericValue(userNumber.charAt(i));
+			}
+			
+			checkFinalResult();
+			
+		}
+		
+	}
+	
+	/**
+	 * Check final result.
+	 */
+	private void checkFinalResult() {
+	
+		this.arrayFinal = new String[4];
+		
+		this.exactDigit = 0;
+		
+		// On parcourt les 2 tableaux et on vérifie chaque entrée
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				
+				if (this.arrayUser[i] == this.arrayRandom[j]) {
+					if (i == j) {
+						this.exactDigit++;
+						this.arrayFinal[i] = " + ";
+						
+						break;
+					}else {
+						if (this.arrayFinal[i] != " + ") {
+							this.arrayFinal[i] = " 1 ";
+						}
+					}
+				}else {
+					if (this.arrayFinal[i] != " + " && this.arrayFinal[i] != " 1 ") {
+						this.arrayFinal[i] = " - ";
+					}
+				}
+				
+			}
+		}
+		
+		displayResult();
+	
+	}
+	
+	/**
+	 * Display result.
+	 */
+	private void displayResult() {
+		
+		// Affichage du résultat final après chaque essai
+		for(int k = 0; k < 4; k++) {
+			System.out.print(this.arrayFinal[k]);
+		}
+		
+		if (this.exactDigit == 4) {
+			this.secsEnd = (new Date().getTime())/1000;
+			System.out.println("\nBravo ! Vous avez gagné en " + this.compt + " coups !");
+			// Soustraction entre secsEnd et secsEarly pour avoir le temps en secondes du jeu
+			System.out.println("Et vous avez mis " + (this.secsEnd - this.secsEarly) + " secondes à trouver!");
+			
+			this.out.println("Gagné en " + this.compt + " coups et " + (this.secsEnd - this.secsEarly) + " secondes");
+			this.out.close();
+			askReplay();
+		}else {
+			this.compt++;
+			if (this.compt == 9) {
+				System.out.println("Vous avez perdu !");
+				
+				this.out.println("Perdu");
+				this.out.close();
+				askReplay();
+			}else {
+				getNumberUser();
+			}
+		}
+		
+	}
+	
+	/**
+	 * Ask replay.
+	 */
+	private void askReplay() {
+		
+		System.out.println("Voulez-vous rejouer? (Y/N)");
+		String rep = sc.nextLine();
+		
+		if (rep.equals("Y") || rep.equals("y")) {
+			play();
+		}else {
+			System.out.println("Fin du jeu !");
+		}
+		
+	}
+	
+	/**
+	 * Check if file exist.
+	 * If yes, use this file and print directly after the existant.
+	 * If not, create new file history.txt.
+	 *
+	 * @throws FileNotFoundException the file not found exception
+	 */
+	private void checkIfFileExist() throws FileNotFoundException {
+		
+		if ( f.exists() && !f.isDirectory() ) {
+		    this.out = new PrintWriter(new FileOutputStream(new File(savestr), true));
+		}
+		else {
+		    this.out = new PrintWriter(savestr);
+		}
+		
+	}
+	
 }
